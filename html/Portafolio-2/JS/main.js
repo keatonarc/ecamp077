767cbfe394b4dfe9061ac89c2776ae06348905e3
keatonarc <145923283+keatonarc@users.noreply.github.com>
2024-08-12 21:42:44 -0400

    port

--- html/Portafolio-2/JS/main.js
+++ html/Portafolio-2/JS/main.js
@@ -20,7 +20,7 @@ function descargarCv(){
   const port = window.location.port;
   console.log('url : ',port);
   const link = document.createElement('a');
-  link.href = 'http://127.0.0.1:'+port+'/html/Portafolio-2/pdf/ejemplo.pdf'; // Reemplaza esto con la ruta a tu archivo PDF
+  link.href = 'http://127.0.0.1:'+port+'/html/Portafolio-2/pdf/ejemplo.pdf'; // Se realiza para poder tomar ruta del dispositivo.
   link.download = 'CV_Matias_Aguilera.pdf'; // Reemplaza esto con el nombre que desees para el archivo descargado
   link.click();
 }
